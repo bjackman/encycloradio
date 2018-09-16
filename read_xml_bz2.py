c632fd814ab4bd6b150f1bd27f75b05e165eb5bb
Brendan Jackman <brendan.jackman@bluwirelesstechnology.com>
2018-09-16 15:49:20 +0100

    Delete even more unused stuff

--- read_xml_bz2.py
+++ read_xml_bz2.py
@@ -46,36 +46,22 @@ def find_listens(page):
     return (t for t in mw.filter_templates() if t.name.lower().startswith("listen"))
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.WARNING)
 
     parser = ArgumentParser()
-    parser.add_argument("xml_bz2_path")
     parser.add_argument("index_sqlite_path")
 
     args = parser.parse_args()
 
     index = WikipediaIndex.from_sqlite_path(args.index_sqlite_path)
-    bz2_file = open(args.xml_bz2_path, "rb")
-    wp_dump = WikipediaDump(bz2_file, index)
 
-    num_pages = 0
-    pages_with_listen = 0
-    for page in wp_dump._iter_pages():
-        num_pages += 1
-        listens = find_listens(page)
-        for listen in listens:
-            filename = listen.get("filename").value.strip()
-            # Not sure why mwparserfromhell gives me "\n" values on the
-            # end. Strip em off.
-            # TODO fix mwparserfromhell.
-            if filename.endswith("\\n"):
-                filename = filename[:-2]
-            index_entry = index.find_page(page.title)
-            if not index_entry:
-                logging.warning("Couldn't find '{}' in index, skipping'"
-                                .format(page.title))
-            else:
+    for page in mwxml.Dump.from_file(sys.stdin):
+        try:
+            listens = find_listens(page)
+            for listen in listens:
+                filename = listen.get("filename").value.strip()
+                index_entry = index.find_page(page.title)
                 index.add_listen(index_entry.id, filename)
-
-        if any(listens):
-            pages_with_listen += 1
+        except Exception as e:
+            if not index_entry:
+                logging.warning(e)
