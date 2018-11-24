b90891775b33d610dd263c7b0cb045729d6516c9
Brendan Jackman <brendan.jackman@bluwirelesstechnology.com>
2018-11-24 14:44:12 +0700

    ch ch ch ch check it

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,7 +3,8 @@ let w = 600;
 let h = 250;
 
 let graph = {
-    nodes: [{}, {}, {}, {}, {}, {}, {}, {}],
+    nodes: [{id: "0"}, {id: "1"}, {id: "2"}, {id: "3"},
+            {id: "4"}, {id: "5"}, {id: "6"}, {id: "7"}],
     links: [
         {source: 0, target: 1},
         {source: 0, target: 2},
@@ -23,10 +24,12 @@ let svg = d3.select("body")
     .attr("height", h);
 
 let simulation = d3.forceSimulation(graph.nodes)
-    .force('charge', d3.forceManyBody())
-    .force('center', d3.forceCenter(w / 2, h / 2))
-    .force('link', d3.forceLink().links(graph.links))
-    .on('tick', onSimTick);
+    .force("charge", d3.forceManyBody())
+    .force("link", d3.forceLink(graph.links))
+    .force("center", d3.forceCenter(w / 2, h / 2))
+    .alphaMin(0.05)
+    .on('tick', onSimTick)
+    .on('end', onSimEnd);
 
 let node = svg.append("g")
     .attr("class", "nodes")
@@ -54,5 +57,37 @@ function onSimTick() {
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
 
-    console.log(link);
+    console.log(simulation.alpha());
+}
+
+function restartSim() {
+    simulation.nodes(graph.nodes);
+    simulation.force('link').links(graph.links);
+    simulation.alpha(0.5).restart();
+}
+
+var done = false;
+
+function onSimEnd() {
+    console.log('end');
+
+    if (done) return;
+    done = true;
+
+    graph.nodes.push({id: "8"});
+    graph.links.push({source: 0, target: 8});
+
+    node = node.data(graph.nodes, d => { return d.id })
+        .enter().append("circle")
+        .attr("r", 5)
+        .merge(node);
+
+    link = link.data(graph.links, d => { [d.source.id, d.target.id].join("-") })
+    link.exit().remove()
+    link = link.enter()
+        .append("line")
+        .attr("stroke", "gray")
+        .merge(link);
+
+    restartSim();
 }
