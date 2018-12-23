2dc516adf02dc34b774ad69b6c2b6e5febbc2b0c
Brendan Jackman <brendan.jackman@bluwirelesstechnology.com>
2018-12-23 16:57:45 +0700

    Improve webpack configuration?????

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devtool: 'inline-source-map',
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
         disableHostCheck: true // I dunno why this is necessary
